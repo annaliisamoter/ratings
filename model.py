05168830f51bd6cd58236b93da97bf87f1180017
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-19 15:32:06 -0700

    Successfully added login to web app.

--- model.py
+++ model.py
@@ -50,15 +50,21 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer,  nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     score = db.Column(db.Integer, nullable=False)
 
-    #movie = db.relationship('Movie')
-    #user = db.relationship('User')
+    movie = db.relationship('Movie', backref=db.backref("ratings", order_by=rating_id))
+    user = db.relationship('User', backref=db.backref("ratings", order_by=rating_id))
 
-    #db.ForeignKey('users.user_id'),
-    #db.ForeignKey('movies.movie_id'),
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        s = "<Rating rating_id=%s movie_id=%s user_id=%s score=%s>"
+        return s % (self.rating_id, self.movie_id, self.user_id,
+                    self.score)
 
 
 
--- server.py
+++ server.py
@@ -2,10 +2,11 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import (Flask, jsonify, render_template,
+    redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Rating, Movie
 
 
 app = Flask(__name__)
@@ -22,8 +23,44 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+    # a = jsonify([1,3])
+    return render_template("homepage.html")
+
+@app.route('/users')
+def user_list():
+    """ Show list of users"""
+
+    users = User.query.all()
+
+    return render_template("user_list.html", users=users)
+
+@app.route('/register', methods=["GET"])
+def register_form():
+    """ renders registration form """
+
+    return render_template("register_form.html")
+
+@app.route('/register', methods=["POST"])
+def register_process():
+    """ checks if user is registered, if not adds new user to database """
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # check if user is already in database, via email
+
+    user_emails = db.session.query(User.email).all()
+
+    if email in user_emails:
+        error_message = "this user already exists"
+        return redirect("/register", message=error_message)
+    else:
+        #do we need to call the set_val_user_id()???
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
@@ -38,5 +75,5 @@ if __name__ == "__main__":
     DebugToolbarExtension(app)
 
 
-    
+
     app.run(port=5000, host='0.0.0.0')
